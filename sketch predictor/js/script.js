f9db305752babd4d77864f4240a28f6fd447d943
somo07 <soumyajitm784@gmail.com>
2023-04-18 08:45:38 +0530

    roughly done...still need a few optimizations

--- sketch predictor/js/script.js
+++ sketch predictor/js/script.js
@@ -9,16 +9,54 @@ class Sketchpad {
     `;
     container.appendChild(this.canvas);
     this.ctx = this.canvas.getContext("2d");
+    this.path;
+    this.isDrawing = false;
     this.#addEventListener();
   }
   #addEventListener() {
     this.canvas.onmousedown = (evt) => {
-      const rect = this.canvas.getBoundingClientRect();
-      const mouse=[
-         Math.round(evt.clientX-rect.left),
-         Math.round(evt.clientY-rect.top)
-      ]
-      console.log(mouse)
+      const mouse = this.#getMouse(evt);
+      // const rect = this.canvas.getBoundingClientRect();
+      // const mouse = [
+      //   Math.round(evt.clientX - rect.left),
+      //   Math.round(evt.clientY - rect.top),
+      // ];
+      // console.log(mouse)
+      this.path = [mouse];
+      this.isDrawing = true;
     };
+    this.canvas.onmousemove = (evt) => {
+      if (this.isDrawing) {
+        //   const rect = this.canvas.getBoundingClientRect();
+        //   const mouse = [
+        //     Math.round(evt.clientX - rect.left),
+        //     Math.round(evt.clientY - rect.top),
+        //   ];
+        // console.log(mouse)
+        const mouse = this.#getMouse(evt);
+        this.path.push(mouse);
+        //   console.log(this.path.length);
+        this.#redraw();
+      }
+    };
+    this.canvas.onmouseup = () => {
+      this.isDrawing = false;
+    };
+  }
+
+  //   redraw
+  #redraw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    //   drawing the path
+    draw.path(this.ctx, this.path);
+  }
+
+  //   method to extract mouse location
+  #getMouse(evt) {
+    const rect = this.canvas.getBoundingClientRect();
+    return [
+      Math.round(evt.clientX - rect.left),
+      Math.round(evt.clientY - rect.top),
+    ];
   }
 }
